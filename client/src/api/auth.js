3e5e9d11d4a3080ba4fb1ae7d51c76301704191e
Josh Eardley <josheardley@gmail.com>
2023-05-22 13:31:20 -0600

    fixes bugs in Login and Register components

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,49 +1,61 @@
-const BASE_URL = `http://localhost.com:8080/api`
 
 export const authenticateUser = async ({email, password}) => {
     try {
-    const response = await fetch(`${BASE_URL}/users/login`, {
+    const response = await fetch(`api/users/login`, {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({email, password})
     });
-    // console.log("RESPONSE", response)
     
         const result = await response.json();
-        console.log("authenticat user result", result);
+        // console.log("authenticate user result", result);
         const {user, message, token}  = result;
         if(token) {
-          localStorage.setItem('token', token);
-          return {user, token, message};
-        } else return {message};
-
-        } catch (err) {
-            console.error(err);
-        }
+            localStorage.setItem('token', token);
+            return {user, token, message};
+        } else return message;
+        
+    } catch (err) {
+        console.error(err);
     }
+}
 
-    export const authenticateNewUser = async ({email, password}) => {
-        try {
-          const response = await fetch(`${BASE_URL}/users/register`, {
+export const authenticateNewUser = async ({email, password}) => {
+    try {
+        const response = await fetch(`api/users/register`, {
             method: "POST",
             headers: {
-              'Content-Type': 'application/json'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({email, password})
-          });
-        //   console.log("RESPONSE", response)
-          
-              const result = await response.json();
-              console.log("authenticate new user result", result);
+        });
+        
+        const result = await response.json();
+        // console.log("authenticate new user result", result);
               const {user, message, token}  = result;
               if(token) {
                 localStorage.setItem('token', token);
                 return {user, token, message};
-              } else return {message};
+              } else return message;
               
           } catch (err) {
               console.error(err);
           }
+      }
+
+      export const getMe = async (token) => {
+        try {
+          const response = await fetch(`api/users/me`, {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            },
+          });
+          const result = await response.json();
+          return result
+        } catch (err) {
+          console.error(err);
+        }
       }
--- server/db/seed.js
+++ server/db/seed.js
@@ -37,8 +37,9 @@ const createTables = async () => {
       id SERIAL PRIMARY KEY,
       email VARCHAR(255) UNIQUE NOT NULL,
       password VARCHAR(255) NOT NULL,
-      address VARCHAR(255) UNIQUE NOT NULL,
-      phoneNumber VARCHAR(15) UNIQUE NOT NULL
+      address VARCHAR(255),
+      phoneNumber VARCHAR(15),
+      UNIQUE(address, phoneNumber)
     );
     CREATE TABLE products(
       id SERIAL PRIMARY KEY,
