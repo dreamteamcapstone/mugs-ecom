706807a04a8e28b4c386d3f8a938e66da44af9cb
Josh Eardley <josheardley@gmail.com>
2023-05-23 08:02:28 -0600

    minor changes, already have been made to main

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import { React, useState } from "react";
+import { NavLink } from "react-router-dom";
 import { authenticateUser } from "../api/auth";
 
 const Login = ({setToken, setIsLoggedIn, setUser}) => {
