fa0d682e189ef29b451c89995b3732daa4c882e0
Courtney Luckett <luckettcourtney@gmail.com>
2023-05-30 09:07:42 -0500

    deleted unused files

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -32,9 +32,9 @@ const Cart = ({ cart, token, setCart, user, setItems, items }) => {
     const updatedProduct = await updateCartItem(
         {id: product.id, quantity: +event.target.value, purchasePrice: product.purchasePrice })
     console.log(updatedProduct);
-      if(!updatedProduct.error & updatedProduct.id) {
-          const updatedOrderItems = items.filter(item => item.id !== product.id)
-          setItems(updatedOrderItems);
+      if(updatedProduct.id) {
+          // const updatedOrderItems = items.filter(item => item.id !== product.id)
+          setItems(items);
       }
   }
 
